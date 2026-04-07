4b367073c32c63e509969f69b3f47630903b7094
Robert Carvalho <roberthcjr1999@gmail.com>
2026-04-06 22:05:59 -0300

    build(migrations): run pending in order

--- migrations/engine/runPendingMigrations.ts
+++ migrations/engine/runPendingMigrations.ts
@@ -4,12 +4,14 @@ export const runPendingMigrations = async (
   migrations: { name: string; version: string }[],
 ) => {
   await db.transaction(async (sql) => {
-    const migrationsToRunPromises = migrations.map(async (migration) => {
-      const fileNameWithoutExtension = `${migration.version}-${migration.name}`;
+    const migrationsInOrder = migrations.sort(
+      (migrationA, migrationB) => +migrationA.version - +migrationB.version,
+    );
+    
+    for (const migration of migrationsInOrder) {
+      const fileNameWithoutExtension = `${migration.version}_${migration.name}`;
       await sql.file(`./migrations/${fileNameWithoutExtension}.sql`);
       await sql`INSERT INTO public.migrations (name) VALUES (${fileNameWithoutExtension})`;
-    });
-
-    await Promise.all(migrationsToRunPromises);
+    }
   });
 };
