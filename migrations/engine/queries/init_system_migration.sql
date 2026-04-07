27d50e2eca1bf7d97e7ba15ab5e5c7065ddbc45a
Robert Carvalho <roberthcjr1999@gmail.com>
2026-04-06 22:07:19 -0300

    build(migrations): remove id from migration table

--- migrations/engine/queries/init_system_migration.sql
+++ migrations/engine/queries/init_system_migration.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS public.migrations (
-  id                UUID          PRIMARY KEY DEFAULT gen_random_uuid(),
-  name              VARCHAR(255)  NOT NULL,
+  name              VARCHAR(255)  PRIMARY KEY,
   runned_at         TIMESTAMPTZ   NOT NULL DEFAULT now()
 );
