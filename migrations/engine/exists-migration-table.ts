66f38da5e86e489f4e1d633062b58e2842146618
Robert Carvalho <roberthcjr1999@gmail.com>
2026-04-07 21:49:08 -0300

    chore(migrations): add to_regclass null type

--- migrations/engine/exists-migration-table.ts
+++ migrations/engine/exists-migration-table.ts
@@ -1,7 +1,7 @@
 import { db } from './config/database';
 
 export const existsMigrationTable = async (): Promise<boolean> => {
-  const [record] = await db<{ to_regclass: string }[]>`
+  const [record] = await db<{ to_regclass: string | null }[]>`
       SELECT to_regclass('public.migrations');
     `;
 
