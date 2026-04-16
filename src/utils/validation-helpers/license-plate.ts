7d88f649ef493eff5340aaa476d1e208cfa6b3e1
Ghabriel Schiffelbein Fiorotti <ghabrielfiorotti@gmail.com>
2026-04-16 11:02:36 -0300

    style: format license-plate validation with oxfmt

--- src/utils/validation-helpers/license-plate.ts
+++ src/utils/validation-helpers/license-plate.ts
@@ -4,7 +4,6 @@ export const licensePlateValidation = z
   .string()
   .transform((val) => val.trim().toUpperCase())
   .refine(
-    (val) =>
-      /^[A-Z]{3}[0-9][A-Z][0-9]{2}$|^[A-Z]{3}[0-9]{4}$/.test(val),
+    (val) => /^[A-Z]{3}[0-9][A-Z][0-9]{2}$|^[A-Z]{3}[0-9]{4}$/.test(val),
     'Invalid license plate format (Mercosul: ABC1D23 or Classic: ABC1234)',
   );
