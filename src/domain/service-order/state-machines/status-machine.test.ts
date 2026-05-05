d15ba3b7468c1743d60442a581b32754688297b3
Robert Carvalho <roberthcjr1999@gmail.com>
2026-05-04 21:49:37 -0300

    style(service-order): apply bun fmt

--- src/domain/service-order/state-machines/status-machine.test.ts
+++ src/domain/service-order/state-machines/status-machine.test.ts
@@ -1,14 +1,15 @@
 import { ServiceOrderStatus } from '@/domain/service-order/types/service-order-status';
-import {
-  determineStatusTransition,
-  StatusDirection,
-} from './status-machine';
+import { determineStatusTransition, StatusDirection } from './status-machine';
 
 describe('service order status machine', () => {
   const cases: Array<
     [ServiceOrderStatus, StatusDirection, ServiceOrderStatus | undefined]
   > = [
-    [ServiceOrderStatus.RECEIVED, StatusDirection.NEXT, ServiceOrderStatus.IN_DIAGNOSTIC],
+    [
+      ServiceOrderStatus.RECEIVED,
+      StatusDirection.NEXT,
+      ServiceOrderStatus.IN_DIAGNOSTIC,
+    ],
     [
       ServiceOrderStatus.IN_DIAGNOSTIC,
       StatusDirection.NEXT,
@@ -19,9 +20,21 @@ describe('service order status machine', () => {
       StatusDirection.NEXT,
       ServiceOrderStatus.IN_EXECUTION,
     ],
-    [ServiceOrderStatus.AWAITING_APPROVAL, StatusDirection.BACK, ServiceOrderStatus.RECEIVED],
-    [ServiceOrderStatus.IN_EXECUTION, StatusDirection.NEXT, ServiceOrderStatus.COMPLETED],
-    [ServiceOrderStatus.COMPLETED, StatusDirection.NEXT, ServiceOrderStatus.DELIVERED],
+    [
+      ServiceOrderStatus.AWAITING_APPROVAL,
+      StatusDirection.BACK,
+      ServiceOrderStatus.RECEIVED,
+    ],
+    [
+      ServiceOrderStatus.IN_EXECUTION,
+      StatusDirection.NEXT,
+      ServiceOrderStatus.COMPLETED,
+    ],
+    [
+      ServiceOrderStatus.COMPLETED,
+      StatusDirection.NEXT,
+      ServiceOrderStatus.DELIVERED,
+    ],
     [ServiceOrderStatus.DELIVERED, StatusDirection.NEXT, undefined],
     [ServiceOrderStatus.RECEIVED, StatusDirection.BACK, undefined],
     [ServiceOrderStatus.CANCELED, StatusDirection.NEXT, undefined],
