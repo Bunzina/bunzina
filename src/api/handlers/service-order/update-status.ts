bbcea12d4e466b9a450f4e26482462f6dbddd48f
Robert Carvalho <roberthcjr1999@gmail.com>
2026-05-11 18:28:50 -0300

    feat(service-order): send email when updating to Awaiting approval

--- src/api/handlers/service-order/update-status.ts
+++ src/api/handlers/service-order/update-status.ts
@@ -1,24 +1,42 @@
 import { UpdateServiceOrderStatusInput } from '@/adapters/input/service-order/update-status';
 import { UpdateServiceOrderStatusUseCase } from '@/application/use-cases/service-order/update-status';
 import { FindServiceOrderByIdUseCase } from '@/application/use-cases/service-order/find-by-id';
+import { FindCustomerByIdUseCase } from '@/application/use-cases/customer/find-by-id';
 import { ServiceOrderRepository } from '@/infrastructure/repositories/service-order/service-order-repository';
+import { CustomerRepository } from '@/infrastructure/repositories/customer/customer-repository';
+import { NotificationService } from '@/infrastructure/services/notification';
+import {
+  DEFAULT_EMAIL,
+  emailTransporter,
+} from '@/infrastructure/configs/email-transporter';
 import { db as dbInstance } from '@/infrastructure/configs/database';
 import logger from '@lucas-pmelo/logger';
 import type { HandlerContext } from '@/api/handler-context';
 
 let serviceOrderRepository: ServiceOrderRepository;
+let customerRepository: CustomerRepository;
 let findServiceOrderByIdUseCase: FindServiceOrderByIdUseCase;
+let findCustomerByIdUseCase: FindCustomerByIdUseCase;
+let notificationService: NotificationService;
 let updateServiceOrderStatusUseCase: UpdateServiceOrderStatusUseCase;
 let updateServiceOrderStatusInput: UpdateServiceOrderStatusInput;
 
 const setDependencies = () => {
   serviceOrderRepository = new ServiceOrderRepository(dbInstance);
+  customerRepository = new CustomerRepository(dbInstance);
   findServiceOrderByIdUseCase = new FindServiceOrderByIdUseCase(
     serviceOrderRepository,
+  );
+  findCustomerByIdUseCase = new FindCustomerByIdUseCase(customerRepository);
+  notificationService = new NotificationService(
+    emailTransporter,
+    DEFAULT_EMAIL,
   );
   updateServiceOrderStatusUseCase = new UpdateServiceOrderStatusUseCase(
     serviceOrderRepository,
     findServiceOrderByIdUseCase,
+    findCustomerByIdUseCase,
+    notificationService,
   );
   updateServiceOrderStatusInput = new UpdateServiceOrderStatusInput(
     updateServiceOrderStatusUseCase,
