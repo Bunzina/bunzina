f88a1d5fdedbdfd5a6a0019e186756baf9bf0abe
Lucas Melo <lucas.pinheiro.melo@gmail.com>
2026-06-28 21:58:58 -0300

    feat: create bunzina app chart

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -9,7 +9,7 @@ resource "aws_ecr_repository" "bunzina" {
 }
 
 resource "aws_ecr_repository" "bunzina_chart" {
-  name                 = "${var.project_name}-chart"
+  name                 = "app-chart"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
 }
